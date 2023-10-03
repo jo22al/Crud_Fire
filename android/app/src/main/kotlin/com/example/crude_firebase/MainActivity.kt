4b61a926181fef689a7bfcb6db19839e2f68783d
jo22al <joeltaguit@gmail.com>
2023-10-03 12:28:38 -0600

    Primer commit

---
+++ android/app/src/main/kotlin/com/example/crude_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crude_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+//Importacion de firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+ runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+
+          ),
+           body: const Center(
+            child: Text('Hello Word'),
+
+           ),
+      )
+    );
+  }
+}
